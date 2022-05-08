d437d9e53b412165987ec21aa726608482b53f4a
unixneo <neo@unix.com>
2022-05-08 13:34:25 +0700

    PROTOTYPE:  Changed some vars to constants.

--- bin/satang_kucoin.rb
+++ bin/satang_kucoin.rb
@@ -10,19 +10,22 @@
 # Use this tool with caution.
 
 def satang(delay=5,thb=20000,fx=34.19,coin='TRX',scan=false,show_btc_thb=false)
-    version="0.0.1.41"
-    
+    VERSION = "0.0.1.41"
+    DELAY = delay
     # Set up Satang Exchange API Parameters
     require 'net/http'
-    source ='https://satangcorp.com/api/orderbook-tickers/'
-    tt_rate=fx
-    thai_baht = thb
+    SATANG_API ='https://satangcorp.com/api/orderbook-tickers/'
+    COIN=coin
+    KUCOIN_BTC_WITHDRAWAL_FEE = 0.0005
+    TT_RATE = fx
+    THAI_BAHT = thb
     # satang pro fee is current 0.2 percent per trade
-    satang_fee = thai_baht *  0.002000
+    SATANG_TRADE_FEE = 0.002000
+    SATANG_FEE = THAI_BAHT * SATANG_TRADE_FEE
 
    
-    satang_pair="#{coin}_THB"
-    kucoin_withdraw_btc_fee = 0.0005
+    SATANG_PAIR="#{COIN}_THB"
+    KUCOIN_BTC_WITHDRAWAL_FEE = 0.0005
    
     # Set up Kucoin Exchange API Parameters
     require 'kucoin'
@@ -32,53 +35,53 @@ def satang(delay=5,thb=20000,fx=34.19,coin='TRX',scan=false,show_btc_thb=false)
         config.passphrase   =   ENV['KUCOIN_API_PASSPHRASE']
     end
 
-    kucoin_pair="#{coin}-BTC"
-    coin_tether="#{coin}-USDT"
+    KUCOIN_PAIR="#{COIN}-BTC"
+    USDT_TETHER="#{COIN}-USDT"
 
     kc_client = Kucoin::Rest::Client.new
     scan_output = [] if scan
     while true do
         output = []
         # Do Satang Exchange API and Simple Math
-        resp = Net::HTTP.get_response(URI.parse(source))
+        resp = Net::HTTP.get_response(URI.parse(SATANG_API))
         data = resp.body
         result = JSON.parse(data)
         
-        if result[satang_pair].nil?
-            puts "ERROR #{coin} NOT AVAILBLE ON SATANG PRO"
+        if result[SATANG_PAIR].nil?
+            puts "ERROR #{COIN} NOT AVAILBLE ON SATANG PRO"
             break
         end
-        coin_thb = ((result[satang_pair]["bid"]['price'].to_f.round(5) + result[satang_pair]["ask"]['price'].to_f.round(5))/2).round(3)
+        coin_thb = ((result[SATANG_PAIR]["bid"]['price'].to_f.round(5) + result[SATANG_PAIR]["ask"]['price'].to_f.round(5))/2).round(3)
         btc_thb = ((result['BTC_THB']["bid"]['price'].to_f.round(5) + result['BTC_THB']["ask"]['price'].to_f.round(5))/2).round(3)
         
         # The network fee for TRX is 1 TRX
         # Needs lookup table for other coins so this formula overestimates the BTC other than TRX right now
         if coin.include? 'TRX'
-            total_coin = (((thai_baht - satang_fee)/ coin_thb) - 1.00000).round(10)
+            total_coin = (((THAI_BAHT - SATANG_FEE)/ coin_thb) - 1.00000).round(10)
         else
-            total_coin = ((thai_baht - satang_fee)/ coin_thb).round(10)
+            total_coin = ((THAI_BAHT - SATANG_FEE)/ coin_thb).round(10)
         end
         
         # Do Kucoin Exchange API and Simple Math
 
-        if  kc_client.ticker(kucoin_pair).nil?
-            puts "ERROR #{coin} PAIR NOT AVAILBLE ON KUCOIN"
+        if  kc_client.ticker(KUCOIN_PAIR).nil?
+            puts "ERROR #{COIN} PAIR NOT AVAILBLE ON KUCOIN"
             break
         end
 
-        coin_btc = kc_client.ticker(kucoin_pair)['price'].to_f.round(10)
+        coin_btc = kc_client.ticker(KUCOIN_PAIR)['price'].to_f.round(10)
        
         # kucoin charges 0.08 percent when paying trading fees with KSC
         btc = coin_btc * total_coin * (1.00000 - 0.0008)
 
         # There is no fee on KUCOIN for receiving coin
-        #trx_fee = kc_client.ticker(coin_tether)['price'].to_f.round(10)
+        #trx_fee = kc_client.ticker(USDT_TETHER)['price'].to_f.round(10)
         
         btc_usd = kc_client.ticker("BTC-USDT")['price'].to_f.round(10)
         total_usd = (btc.round(10) * btc_usd.round(10)).round(2)
 
         if show_btc_thb
-            btc_to_satang = btc.round(10) -kucoin_withdraw_btc_fee.round(10)
+            btc_to_satang = btc.round(10) - KUCOIN_BTC_WITHDRAWAL_FEE.round(10)
             final_thb_from_btc = ((btc_to_satang.round(10) *  btc_thb.round(10)) * (1.0000 -0.0025)).round(10)
         end
 
@@ -90,24 +93,24 @@ def satang(delay=5,thb=20000,fx=34.19,coin='TRX',scan=false,show_btc_thb=false)
         else
             output << "-------------------------------"
             output <<  "TIME #{Time.now}"
-            output <<  "USD #{"%.2f" % (thai_baht / tt_rate).round(2)}" 
-            output <<  "#{coin}-THB #{coin_thb}" 
-            output <<  "TOTAL #{coin} #{total_coin.round(3)}" 
-            output <<  "#{coin}-BTC #{"%.9f" %  coin_btc.to_f.round(9)}"
+            output <<  "USD #{"%.2f" % (THAI_BAHT / TT_RATE ).round(2)}" 
+            output <<  "#{COIN}-THB #{coin_thb}" 
+            output <<  "TOTAL #{COIN} #{total_coin.round(3)}" 
+            output <<  "#{COIN}-BTC #{"%.9f" %  coin_btc.to_f.round(9)}"
             output <<  "BTC-USD #{"%.2f" % btc_usd.round(2)}"
             if show_btc_thb
-                output <<  "KUCOIN BTC WITHDRAWAL FEE #{"%.2f" % (btc_usd.round(2) * kucoin_withdraw_btc_fee.round(10)).round(2)}"
+                output <<  "KUCOIN BTC WITHDRAWAL FEE #{"%.2f" % (btc_usd.round(2) * KUCOIN_BTC_WITHDRAWAL_FEE.round(10)).round(2)}"
             end
-            output <<  "TOTAL BTC #{btc.round(10)} (#{coin})"
+            output <<  "TOTAL BTC #{btc.round(10)} (#{COIN})"
             output <<  "BTC-THB #{btc_thb}"
             output <<  "TOTAL USD #{"%.2f" %  total_usd }"
-            output <<  "TOTAL THB #{"%.2f" % (total_usd  * tt_rate).round(2)}"
+            output <<  "TOTAL THB #{"%.2f" % (total_usd  * TT_RATE ).round(2)}"
             if show_btc_thb
                 output <<  "TOTAL BTC TO SATANG #{"%.9f" % btc_to_satang.round(10)}"
                 output <<  "TOTAL BTC_THB #{"%.9f" % final_thb_from_btc.round(10)}"
             end
             puts output
-            sleep delay
+            sleep DELAY
         end
     end
     return scan_output if scan
